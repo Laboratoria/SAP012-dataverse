f4e53448a2c51d3c1741184f124774797910cb3e
flaviane-lima <rflaviane41@gmail.com>
2024-01-24 20:23:04 -0300

    retirou os join que não era mais necessário

--- src/view.js
+++ src/view.js
@@ -3,25 +3,25 @@ export const renderItems = (data) => {
   // Aquí comienza tu código y puedes retornar lo que tu necesites
   let cartoes = "";
   for (let i = 0; i < data.length; i++) {
-    cartoes  += `<li class='cartao' itemscope itemtype='${data[i].id}'>`;
-    cartoes  += `<section class='info-externa'>`;
-    cartoes  += `<p>Classificação: <span itemprop='classification'>${data[i].extraInfo.classification}</span></p>`;
-    cartoes  += `<img ${data[i].imageUrl}' alt='capa do anime ${data[i].name}'>`;
-    cartoes  += `<h5><span itemprop='name'>${data[i].name}</span></h5>`;
-    cartoes  += `<p>Avaliação: <span itemprop='assessment'>${data[i].extraInfo.assessment}</span></p>`;
-    cartoes  += `<p><span itemprop='streaming'>${data[i].extraInfo.streaming}</span></p>`;
-    cartoes  += `<p>Temporadas: <span itemprop='seasons'>${data[i].extraInfo.seasons}</span></p>`;
-    cartoes  += `<button onclick='openModal()'>Curiosidades</button></section>`;
+    cartoes  +=   `<li class="cartao" itemscope itemtype="${data[i].id}">
+                  <section class="info-externa">
+                  <p><span itemprop="classification">${data[i].extraInfo.classification}</span></p>
+                  <figure> <img  itemprop="imageUrl" src="${data[i].imageUrl}"  title="${data[i].name}"></figure>
+                  <h5><span itemprop="name">${data[i].name}</span></h5>
+                  <p><span itemprop="assessment">${data[i].extraInfo.assessment}</span></p>
+                  <p><span itemprop="streaming">${data[i].extraInfo.streaming}</span></p>
+                  <p>Temporadas <span itemprop="seasons">${data[i].extraInfo.seasons}</span></p>
+                  <button onclick="openModal()">Curiosidades</button></section>
 
-    cartoes  += `<section class='curiosidades' id='modal-${data[i].id}'>`;
-    cartoes  += `<span class='close' onclick='closeModal()'>&times;</span>`;
-    cartoes  += `<h6><span itemprop='shortDescription'>${data[i].shortDescription.join(', ')}</span></h6>`;
-    cartoes  += `<p><span itemprop='description'>${data[i].description}</span>.</p>`;
-    cartoes  += `<p>Episódios: <span itemprop='episodes'>${data[i].extraInfo.episodes}</span></p>`;
-    cartoes  += `<p>Canção: <span itemprop='song'>${data[i].extraInfo.song.join(', ')}</span></p>`;
-    cartoes  += `<p>Criador: <span itemprop='creator'>${data[i].facts.creator}</span></p>`;
-    cartoes  += `<p>Produtor: <span itemprop='producer'>${data[i].facts.producer.join(', ')}</span></p>`;
-    cartoes  += `<p>Studio: <span itemprop='studio'>${data[i].facts.studio.join(', ')}</span></p></section></li>`;
+                  <section class="curiosidades" id="modal-${data[i].id}">
+                  <span class="close" onclick="closeModal()">&times;</span>
+                  <h6><span itemprop="shortDescription">${data[i].shortDescription}</span></h6>
+                  <p><span itemprop="description">${data[i].description}</span>.</p>
+                  <p>Episódios: <span itemprop="episodes">${data[i].extraInfo.episodes}</span></p>
+                  <p>Canção: <span itemprop="song">${data[i].extraInfo.song.join(", ")}</span></p>
+                  <p>Criador: <span itemprop="creator">${data[i].facts.creator}</span></p>
+                  <p>Produtor: <span itemprop="producer">${data[i].facts.producer}</span></p>
+                  <p>Studio: <span itemprop="studio">${data[i].facts.studio}</span></p></section></li>`;
   } 
   return cartoes ;
 };
