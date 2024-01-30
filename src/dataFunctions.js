e0afdfbcf305038e7524bf045da3e9943b58841f
flaviane-lima <rflaviane41@gmail.com>
2024-01-30 19:06:56 -0300

    criei a função de filtrar e ordenar

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,9 +1,43 @@
 // Estas funciones son ejemplos, aquí puedes desarrollar tus propias funciones.
 
-export const example = () => {
-  return 'example';
+export const filterBy = (data, filterBy, value) => {
+  const itemSelecionado = data.filter(item => {
+    if ('extraInfo' in item && filterBy in item.extraInfo)
+      return item.extraInfo[filterBy] === value
+  });
+
+  return itemSelecionado;
+
 };
 
-export const anotherExample = () => {
-  return [];
+export const sortBy = (data, sortBy, sortOrder ) => {  
+  const ordenacao = data.sort((a, b) => {
+    let result = 0;
+
+    if (a[sortBy] < b[sortBy]) {
+      if (sortOrder === 'asc') {
+        result = -1;
+      } else {
+        result = 1;
+      }
+    } else if (a[sortBy] > b[sortBy]) {
+      if (sortOrder === 'asc') {
+        result = 1;
+      } else {
+        result = -1;
+      }
+    }
+
+    return result;
+  });
+
+  // if (sortOrder === 'desc') {
+  //   ordenacao.reverse();
+  // }
+
+  return ordenacao;
 };
+
+// export const computeStates = (data) => {
+//   return [];
+// };
