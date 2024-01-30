2c72352f9c19f076eeac2b88a71ed35faa1ec628
flaviane-lima <rflaviane41@gmail.com>
2024-01-30 19:10:26 -0300

    foi feita a manipulação de dom dos cartões, funções e botões

--- src/main.js
+++ src/main.js
@@ -1,15 +1,45 @@
-import { example } from './dataFunctions.js';
+import { filterBy, sortBy } from './dataFunctions.js';
 import { renderItems } from './view.js';
-
 import data from './data/dataset.js';
 
+let dadosExibidos = data;
 
+const listaCartao = document.querySelector('#root');
 
+document.addEventListener("DOMContentLoaded", () => {
+  listaCartao.appendChild(renderItems(data))
+})
 
+// filtrar os dados
+const botaoFiltra = document.getElementById('streaming');
 
-const listaCartao = document.getElementById('cartoes');
-listaCartao.innerHTML=renderItems(data);
+botaoFiltra.addEventListener('change', (event) => {
 
+  const valorFiltro = event.target.value;
 
+  dadosExibidos = filterBy(data, 'streaming', valorFiltro)
+  listaCartao.innerHTML = ""
+  listaCartao.appendChild(renderItems(dadosExibidos))
+})
 
-//console.log(example, renderItems(data), data);
+// limpar  area filtrada
+const limparBotao = document.getElementById('limpar-filtro');
+
+limparBotao.addEventListener('click', () => {
+  listaCartao.innerHTML = ""
+  listaCartao.appendChild(renderItems(data))
+})
+
+// codigo de ordenação
+
+const ordenacao = document.getElementById('ordenacao');
+
+ordenacao.addEventListener('change', (event) => {
+  const valorOrdenacao = event.target.value;
+  const valorOrdenado = sortBy(dadosExibidos,'name',valorOrdenacao)
+  listaCartao.innerHTML = ""
+  listaCartao.appendChild(renderItems(valorOrdenado))
+  // console.log(data,'name',valorOrdenacao, dadosExibidos)
+})
+
+// console.log(example, renderItems(data), data);
